e0fbf4ad8fbf8f098e7553ceac970f877fa5521f
adrianperras <aperr109@uottawa.ca>
2024-10-07 08:32:43 -0400

    replaced files witih updated versions

--- lab2/delay.asm
+++ lab2/delay.asm
@@ -1,12 +1,15 @@
 ;------------------------------------------------------
-; Alarm System Simulation Assembler Program
+; Alarm System Assembler Program
 ; File: delay.asm
 ; Description: The Delay Module
 ; Author: Gilbert Arbez
 ; Date: Fall 2010
 ;------------------------------------------------------
+ include "sections.inc"
+ include "alarm.inc"
 
 ; Some definitions
+MSCOUNT equ 3000
 
 	SWITCH code_section
 
@@ -16,20 +19,12 @@
 ; Returns: nothing
 ; Description: Delays for num ms. 
 ;--------------------------------
-delayms: 
-	pshx 	; save register contents
-	pshy
-
-	bsr setDelay ; set our delay
-
-loop:
-	bsr polldelay	; check if the delay is over
-	cmpa #TRUE		; check return value with true
-	bne loop			; if the delay isn't finished loop back
-	
-	; done looping
-	puly	; restore register contents
-	pulx
+delayms: pshd  
+dlms_while:           ; do
+   jsr delay1ms       ; delay1ms()
+   dbne d,dlms_while  ; num--; while(num !=0)
+dlms_endwhile:
+   puld            ; restores register
    rts
 
 ;------------------------------------------------------
@@ -41,7 +36,7 @@ loop:
 ;              variable.
 ;------------------------------------------------------
 setDelay: 
-	std delayCount
+   std delayCount     ; delayCount = cnt;
    rts
 
 
@@ -55,6 +50,32 @@ setDelay:
 ;      delayCount
 ; Description: The subroutine delays for 1 ms, decrements delayCount.
 ;              If delayCount is zero, return TRUE; FALSE otherwise.
+;------------------------------------------------------
+; Stack Usage:
+	OFFSET 0  ; to setup offset into stack
+PDLY_VARSIZE:
+PDLY_PR_X   DS.W 1 ; preserve X
+PDLY_RA     DS.W 1 ; return address
+
+polldelay:  pshx
+   ldaa #FALSE	   ; byte retval=FALSE; // return value
+   jsr delay1ms	   ; delay1ms();
+   ldx delayCount    ; delayCount--;
+   dex
+   stx delayCount
+pld_if:
+   bne pld_endif    ; if(delayCount==0) 
+   ldaa #TRUE        ; retval=TRUE;
+pld_endif:
+   ; restore registers and stack
+   pulx
+   rts
+
+;------------------------
+; Subroutine: delay1ms
+; Parameters:  none
+; Returns: nothing
+; Description: The subroutine delays for 1 ms and returns.      
 ;   Core Clock is set to 24 MHz, so 1 cycle is 41 2/3 ns
 ;   NOP takes up 1 cycle, thus 41 2/3 ns
 ;   Need 24 cyles to create 1 microsecond delay
@@ -63,53 +84,27 @@ setDelay:
 ;	BNE - 3 cyles - when branch is taken
 ;	Need 4 NOP
 ;   Run Loop 3000 times to create a 1 ms delay   
-;------------------------------------------------------
+;---------------------------
 ; Stack Usage:
 	OFFSET 0  ; to setup offset into stack
-PDLY_VARSIZE:
-PDLY_PR_Y   DS.W 1 ; preserve Y
-PDLY_PR_X   DS.W 1 ; preserve X
-PDLY_PR_B   DS.B 1 ; preserve B
-PDLY_RA     DS.W 1 ; return address
+DLY1_VARSIZE:
+DLY1_PR_X   DS.W 1 ; preserve X
+DLY1_RA     DS.W 1 ; return address
 
-delay1ms:
-	nop
-	nop
-	nop
-	nop
-	dex
-	bne delay1ms
-	rts
-
-polldelay: pshb
-   pshx
-   pshy
-
-	; Use the subroutine developed in lab 1
-   ldx #3000
-   jsr delay1ms
-   ldd delayCount
-	subd #1
-	std delayCount
-	cpd #0
-	beq endDelay
-	bra contDelay
-
-contDelay:
-	ldaa #FALSE
-	puly
-	pulx
-	pulb
-	rts
-   
-endDelay:
-	ldaa #TRUE
-   puly
+delay1ms: pshx
+   ldx #MSCOUNT	   ; byte cntr = MSCOUNT;
+dly1_while:
+   beq dly1_endwhile ; while(cntr != 0)
+                     ; {
+   nop               ;   asm { nop; nop; nop; nop; }
+   nop
+   nop
+   nop
+   dex               ;   cntr--;
+   bra dly1_while    ; }
+dly1_endwhile:
    pulx
-   pulb
    rts
-
-
 
 ;------------------------------------------------------
 ; Global variables
--- lab2/keyPad.asm
+++ lab2/keyPad.asm
@@ -36,6 +36,30 @@ NOKEY		EQU 	$00   ; No key pressed during poll period
 POLLCOUNT	EQU	1     ; Number of loops to create 1 ms poll time
 
  SWITCH globalConst  ; Constant data
+ 
+ ; defnitions for structure cnvTbl_struct
+ OFFSET 0
+cnvTbl_code ds.b 1
+cnvTbl_ascii  ds.b 1
+cnvTbl_struct_len EQU *
+
+; Conversion Table
+cnvTbl  dc.b %11101110,'1'
+	dc.b %11101101,'2'
+	dc.b %11101011,'3'
+	dc.b %11100111,'a'
+	dc.b %11011110,'4'
+	dc.b %11011101,'5'
+	dc.b %11011011,'6'
+	dc.b %11010111,'b'
+	dc.b %10111110,'7'
+	dc.b %10111101,'8'
+	dc.b %10111011,'9'
+	dc.b %10110111,'c'
+	dc.b %01111110,'*'
+	dc.b %01111101,'0'
+	dc.b %01111011,'#'
+	dc.b %01110111,'d'
 
 
 
@@ -47,6 +71,8 @@ POLLCOUNT	EQU	1     ; Number of loops to create 1 ms poll time
 ; 	Initiliases PORT A
 ;-----------------------------------------------------------	
 initKeyPad:
+	movb #$F0, DDRA
+	bset PUCR, $01
 	rts
 
 ;-----------------------------------------------------------    
@@ -68,6 +94,29 @@ initKeyPad:
 
 pollReadKey: 
 
+   pshx
+   ldy POLLCOUNT
+   movb #$0F, PORTA
+   
+prk_mainloop:
+prk_if1:
+   lda PORTA
+   cmpa #$0F
+   beq endloops
+   ldx #3000
+   ldd #1
+   jsr delayms
+prk_if2:
+   lda PORTA
+   cmpa #$0F
+   beq endloops
+   jsr readKey
+
+endloops:
+   dey
+   bne prk_mainloop
+   pulx
+
    rts
 
 ;-----------------------------------------------------------	
@@ -83,10 +132,114 @@ pollReadKey:
 ;  translate to get the corresponding ASCII code.
 ;-----------------------------------------------------------	
 ; Stack Usage
-	OFFSET 0  ; to setup offset into stack
+; OFFSET 0  ; to setup offset into stack
+COMPARE_KEY DS.B 1
+A_STORE DS.B 1
 
 readKey:
+	psha
 
-    rts		           ;  return(ch); 
+rk_loop:
+	movb #$0F, PORTA	; init port A
+	ldd #10		; delay for 10 ms
+	jsr delayms
+	ldab PORTA			; save the current value of PORTA into B
+	cmpb #$0F			; check if we have input
+	beq rk_loop			; if we don't have input
+		; call our function to delay
+	jsr readKeyboard
 
+
+	jsr translate
+
+	pula
+
+ rts		           ;  return(ch); 
+
+
+;-----------------------------------------------------------	
+; Subroutine: ch <- readKey
+; Arguments: none
+; Local variable: 
+;	ch - ASCII Code in accumulator B
+
+; Description:
+;  Main subroutine that reads a code from the
+;  keyboard using the subroutine readKeybrd.  The
+;  code is then translated with the subroutine
+;  translate to get the corresponding ASCII code.
+;-----------------------------------------------------------
+
+readKeyboard:
+	movb #%11101111, PORTA
+
+if1: ldab PORTA
+	cmpb #%11101111
+	bne endloop
+	movb #%11011111, PORTA
+
+if2: ldab PORTA
+	cmpb #%11011111
+	bne endloop
+	movb #%10111111, PORTA
+
+if3: ldab PORTA
+	cmpb #%10111111
+	bne endloop
+	movb #%01111111, PORTA
+	
+endloop:
+	ldab PORTA
+	rts
+	
+;-----------------------------------------------------------	
+; Subroutine:  ch <- translate(code)
+; Arguments
+;	code - in Acc B - code read from keypad port
+; Returns
+;	ch - saved on stack but returned in Acc B - ASCII code
+; Local Variables
+;    	ptr - in register X - pointer to the table
+;	count - counter for loop in accumulator A
+; Description:
+;   Translates the code by using the conversion table
+;   searching for the code.  If not found, then BADCODE
+;   is returned.
+;-----------------------------------------------------------	
+; Stack Usage:
+   OFFSET 0
+TR_CH DS.B 1  ; for ch 
+TR_PR_A DS.B 1 ; preserved regiters A
+TR_PR_X DS.B 1 ; preserved regiters X
+TR_RA DS.W 1 ; return address
+
+translate: psha
+	pshx	; preserve registers
+	leas -1,SP 		    ; byte chascii;
+	ldx #cnvTbl		    ; ptr = cnvTbl;
+	clra			    ; ix = 0;
+	movb #BADCODE,TR_CH,SP ; ch = BADCODE;
+
+TR_loop 			    ; do {
+
+	; IF code = [ptr]
+	cmpb cnvTbl_code,X  	    ;     if(code == ptr->code)
+	bne TR_endif
+				    ;     {
+	movb cnvTbl_ascii,X,TR_CH,SP ;        ch <- [ptr+1]
+	bra TR_endwh  		    ;         break;
+TR_endif  			    ;     }
+				    ;     else {	
+	leax cnvTbl_struct_len,X    ;           ptr++;
+	inca ; increment count      ;           ix++;
+                                    ;     }	
+	cmpa #NUMKEYS               ;} WHILE count < NUMKEYS
+	blo TR_LOOP	
+tr_endwh ; ENDWHILE
+
+	pulb ; move ch to Acc B
+	; restore registres
+	pulx
+	pula
+	rts
 
